d22fc37b9891d5bdbf74b685f78d63be0d8f31ce
Roby-diamond <martinez.robyp@gmail.com>
2022-02-28 14:00:08 -0700

    adds navigation and home page

--- App.js
+++ App.js
@@ -1,35 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, ActivityIndicator, ImageBackground } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const URL = 'https://api.openbrewerydb.org/breweries?by_city=fort_collins';
+const Stack = createNativeStackNavigator();
+const image= { uri: 'https://images.unsplash.com/photo-1584225065152-4a1454aa3d4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'}
 
-export default function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [breweries, setBreweries] = useState([])
-
-  const getBreweries = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    setBreweries(data);
-    setIsLoading(false)
-  }
-
-  useEffect(() => getBreweries(), [])
-
+function HomeScreen() {
+  
   return (
-    <SafeAreaView style={styles.container}>
-      {isLoading ? <ActivityIndicator /> :
-        <FlatList
-          data={breweries}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text style={styles.item}>{item.name}</Text>
-          )}
-        />
-      }
-      <StatusBar style="auto" />
-    </SafeAreaView>
+  <View style={styles.container}>
+    <ImageBackground source={image} style={{width: '100%', height: '100%'}}
+  ></ImageBackground>
+  </View>)
+}
+
+function Main() {
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [breweries, setBreweries] = useState([])
+
+    const getBreweries = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setBreweries(data);
+      setIsLoading(false)
+      
+      return (
+      <SafeAreaView style={styles.container}>
+        {isLoading ? <ActivityIndicator /> :
+          <>
+            { }
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={breweries}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                <Text style={styles.item}>
+                  {item.name} {'\n'} {item.street}
+                </Text>
+              )}
+            />
+          </>
+        }
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    )
+    }
+
+    useEffect(() => getBreweries(), [])
+}
+ export default function App() {
+    
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -39,14 +70,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   item: {
-    flex: 1,
-    margin:10,
+    backgroundColor: '#a0c5e8',
+    borderRadius: 7,
+    margin: 10,
+    padding: 30,
     alignSelf: 'stretch',
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey',
     justifyContent: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+  },
+  image: {
+    flex:1,
+    justifyContent:'center',
+    resizeMode: 'cover'
   }
 });
