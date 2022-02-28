dc815c123aa72710912069891b1ce2a80c1d3430
Roby-diamond <martinez.robyp@gmail.com>
2022-02-28 11:42:29 -0700

    sets up basic styling

--- App.js
+++ App.js
@@ -1,40 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, ActivityIndicator } from 'react-native';
 
 const URL = 'https://api.openbrewerydb.org/breweries?by_city=fort_collins';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
   const [breweries, setBreweries] = useState([])
 
   const getBreweries = async () => {
     const response = await fetch(URL);
     const data = await response.json();
     setBreweries(data);
+    setIsLoading(false)
   }
 
   useEffect(() => getBreweries(), [])
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <FlatList
+    <SafeAreaView style={styles.container}>
+      {isLoading ? <ActivityIndicator /> :
+        <FlatList
           data={breweries}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Text>{item.name}</Text>
-            )}
-            />
+            <Text style={styles.item}>{item.name}</Text>
+          )}
+        />
+      }
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    flex: 1,
+    margin:10,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    justifyContent: 'center',
+    textAlign: 'center'
+  }
 });
