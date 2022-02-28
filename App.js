22cc50bcb65b2a3a62520eda90f310c41ab9c3c6
Roby-diamond <martinez.robyp@gmail.com>
2022-02-28 09:05:37 -0700

    connects openbrewerydb api

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const URL = 'https://api.openbrewerydb.org/breweries?by_city=fort_collins';
 
 export default function App() {
+  const [breweries, setBreweries] = useState([])
+
+  const getBreweries = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setBreweries(data);
+  }
+
+  useEffect(() => getBreweries(), [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+          data={breweries}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+            )}
+            />
       <StatusBar style="auto" />
     </View>
   );
